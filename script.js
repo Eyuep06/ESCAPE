59da2670e986be5dd0f53259327718f1f43d5217
Eyuep06 <80841485+Eyuep06@users.noreply.github.com>
2023-01-16 18:41:11 +0100

    fast_fertig

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ var ESCAPE;
     let startButton = document.getElementById("startButton");
     let buttonClickedControl = 0; //Um den Countdown zu stoppen damit kein Alert kommt
     let auswahlBereich = document.getElementById("auswahl");
+    let countdownBereich = document.querySelector(".countdown");
+    let countdownBereich2 = document.querySelector(".countdown2");
+    let countdownBereich3 = document.querySelector(".countdown3");
+    let secondsLeft = 10;
+    let secondsLeft2 = 10;
+    let secondsLeft3 = 10;
     startButton.addEventListener("click", clickStartButton);
     function clickStartButton() {
         let feedbackOnClick = new Audio("583765__wearefreesfx__cinematic-trailer-whoosh-transition.wav");
@@ -18,30 +24,116 @@ var ESCAPE;
     }
     function szeneZweiA() {
         buttonClickedControl++;
+        countdownBereich.remove();
         clearButtons();
-        let trackSzeneZweiA = new Audio("Tracks/Szene2A.mp4");
+        let trackSzeneZweiA = new Audio("Tracks/Szene2A.ogg");
         trackSzeneZweiA.play();
+        setTimeout(createButtons2, 35000);
     }
     function szeneZweiB() {
         buttonClickedControl++;
         clearButtons();
-        let trackSzeneZweiA = new Audio("Tracks/Szene2B.mp4");
-        trackSzeneZweiA.play();
+        let trackSzeneZweiB = new Audio("Tracks/Szene2B.mp4");
+        trackSzeneZweiB.play();
+    }
+    function szeneDreiA() {
+        buttonClickedControl++;
+        countdownBereich2.remove();
+        clearButtons();
+        let trackSzeneDreiA = new Audio("Tracks/Szene3A.ogg");
+        trackSzeneDreiA.play();
+        setTimeout(createButtons3, 38000);
+    }
+    function szeneDreiB() {
+        buttonClickedControl++;
+        countdownBereich2.remove();
+        clearButtons();
+        let trackSzeneDreiB = new Audio("Tracks/Szene3B.mp4");
+        trackSzeneDreiB.play();
+    }
+    function szeneVierA() {
+        buttonClickedControl++;
+        countdownBereich3.remove();
+        clearButtons();
+        let trackSzeneVierA = new Audio("Tracks/Szene4A.ogg");
+        trackSzeneVierA.play();
+        //setTimeout(createButtons3, 35000);
+    }
+    function szeneVierB() {
+        buttonClickedControl++;
+        countdownBereich3.remove();
+        clearButtons();
+        let trackSzeneVierB = new Audio("Tracks/Szene4B.mp4");
+        trackSzeneVierB.play();
     }
     function createButtons1() {
         let opt1 = document.createElement("button");
-        opt1.innerHTML = "Auswahl 1";
+        opt1.innerHTML = "Einsteigen";
         opt1.id = "opt1";
         let opt2 = document.createElement("button");
-        opt2.innerHTML = "Auswahl 2";
+        opt2.innerHTML = "Nicht Einsteigen";
         opt2.id = "opt2";
         auswahlBereich.appendChild(opt1);
         auswahlBereich.appendChild(opt2);
         opt1.addEventListener("click", szeneZweiA);
         opt2.addEventListener("click", szeneZweiB);
+        setInterval(countdown, 1000);
+    }
+    function createButtons2() {
+        let opt1 = document.createElement("button");
+        opt1.innerHTML = "Sterben lassen";
+        opt1.id = "opt1";
+        let opt2 = document.createElement("button");
+        opt2.innerHTML = "Für Rückkehr einsetzen";
+        opt2.id = "opt2";
+        auswahlBereich.appendChild(opt1);
+        auswahlBereich.appendChild(opt2);
+        opt1.addEventListener("click", szeneDreiA);
+        opt2.addEventListener("click", szeneDreiB);
+        setInterval(countdown2, 1000);
+    }
+    function createButtons3() {
+        let opt1 = document.createElement("button");
+        opt1.innerHTML = "Nicht springen";
+        opt1.id = "opt1";
+        let opt2 = document.createElement("button");
+        opt2.innerHTML = "Springen";
+        opt2.id = "opt2";
+        auswahlBereich.appendChild(opt1);
+        auswahlBereich.appendChild(opt2);
+        opt1.addEventListener("click", szeneVierA);
+        opt2.addEventListener("click", szeneVierB);
+        setInterval(countdown3, 1000);
     }
     function clearButtons() {
         auswahlBereich.innerHTML = "";
+    }
+    function countdown() {
+        if (secondsLeft > 0 && buttonClickedControl == 0) {
+            secondsLeft--;
+            countdownBereich.innerHTML = "" + secondsLeft;
+        }
+        if (secondsLeft == 0) {
+            alert("Game Over");
+        }
+    }
+    function countdown2() {
+        if (secondsLeft2 > 0 && buttonClickedControl == 1) {
+            secondsLeft2--;
+            countdownBereich2.innerHTML = "" + secondsLeft2;
+        }
+        if (secondsLeft2 == 0) {
+            alert("Game Over");
+        }
+    }
+    function countdown3() {
+        if (secondsLeft3 > 0 && buttonClickedControl == 2) {
+            secondsLeft3--;
+            countdownBereich3.innerHTML = "" + secondsLeft3;
+        }
+        if (secondsLeft3 == 0) {
+            alert("Game Over");
+        }
     }
 })(ESCAPE || (ESCAPE = {}));
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -2,51 +2,129 @@ namespace ESCAPE {
     let startButton: HTMLElement = document.getElementById("startButton") as HTMLElement;
     let buttonClickedControl: number = 0; //Um den Countdown zu stoppen damit kein Alert kommt
     let auswahlBereich: HTMLElement = document.getElementById("auswahl") as HTMLElement;
+    let countdownBereich: HTMLElement = document.querySelector(".countdown") as HTMLElement;
+    let countdownBereich2: HTMLElement = document.querySelector(".countdown2") as HTMLElement;
+    let countdownBereich3: HTMLElement = document.querySelector(".countdown3") as HTMLElement;
+    let secondsLeft: number = 10;
+    let secondsLeft2: number = 10;
+    let secondsLeft3: number = 10;
 
     startButton.addEventListener("click", clickStartButton);
 
     function clickStartButton(): void {
-        
+
         let feedbackOnClick: HTMLAudioElement = new Audio("583765__wearefreesfx__cinematic-trailer-whoosh-transition.wav");
         feedbackOnClick.play();
         startButton.remove();
         setTimeout(szeneEins, 2000);
     }
 
- 
+
     function szeneEins(): void {
         let trackSzeneEins: HTMLAudioElement = new Audio("Tracks/Szene1.mp4");
         trackSzeneEins.play();
         setTimeout(createButtons1, 38000);
-        
     }
 
     function szeneZweiA(): void {
-        buttonClickedControl ++;
+        buttonClickedControl++;
+        countdownBereich.remove();
         clearButtons();
-        let trackSzeneZweiA: HTMLAudioElement = new Audio("Tracks/Szene2A.mp4");
+        let trackSzeneZweiA: HTMLAudioElement = new Audio("Tracks/Szene2A.ogg");
         trackSzeneZweiA.play();
+        setTimeout(createButtons2, 35000);
     }
 
     function szeneZweiB(): void {
-        buttonClickedControl ++;
+        buttonClickedControl++;
         clearButtons();
-        let trackSzeneZweiA: HTMLAudioElement = new Audio("Tracks/Szene2B.mp4");
-        trackSzeneZweiA.play();
+        let trackSzeneZweiB: HTMLAudioElement = new Audio("Tracks/Szene2B.mp4");
+        trackSzeneZweiB.play();
     }
 
-    function createButtons1 (): void {
+    function szeneDreiA(): void {
+        buttonClickedControl++;
+        countdownBereich2.remove();
+        clearButtons();
+        let trackSzeneDreiA: HTMLAudioElement = new Audio("Tracks/Szene3A.ogg");
+        trackSzeneDreiA.play();
+        setTimeout(createButtons3, 38000);
+    }
+
+    function szeneDreiB(): void {
+        buttonClickedControl++;
+        countdownBereich2.remove();
+        clearButtons();
+        let trackSzeneDreiB: HTMLAudioElement = new Audio("Tracks/Szene3B.mp4");
+        trackSzeneDreiB.play();
+    }
+
+    function szeneVierA(): void {
+        buttonClickedControl++;
+        countdownBereich3.remove();
+        clearButtons();
+        let trackSzeneVierA: HTMLAudioElement = new Audio("Tracks/Szene4A.ogg");
+        trackSzeneVierA.play();
+        //setTimeout(createButtons3, 35000);
+    }
+
+    function szeneVierB(): void {
+        buttonClickedControl++;
+        countdownBereich3.remove();
+        clearButtons();
+        let trackSzeneVierB: HTMLAudioElement = new Audio("Tracks/Szene4B.mp4");
+        trackSzeneVierB.play();
+    }
+    
+
+    function createButtons1(): void {
         let opt1: HTMLElement = document.createElement("button");
-        opt1.innerHTML = "Auswahl 1";
+        opt1.innerHTML = "Einsteigen";
         opt1.id = "opt1";
         let opt2: HTMLElement = document.createElement("button");
-        opt2.innerHTML = "Auswahl 2";
+        opt2.innerHTML = "Nicht Einsteigen";
         opt2.id = "opt2";
         auswahlBereich.appendChild(opt1);
         auswahlBereich.appendChild(opt2);
 
         opt1.addEventListener("click", szeneZweiA);
         opt2.addEventListener("click", szeneZweiB);
+        setInterval(countdown, 1000);
+
+
+    }
+
+    function createButtons2(): void {
+        let opt1: HTMLElement = document.createElement("button");
+        opt1.innerHTML = "Sterben lassen";
+        opt1.id = "opt1";
+        let opt2: HTMLElement = document.createElement("button");
+        opt2.innerHTML = "Für Rückkehr einsetzen";
+        opt2.id = "opt2";
+        auswahlBereich.appendChild(opt1);
+        auswahlBereich.appendChild(opt2);
+
+        opt1.addEventListener("click", szeneDreiA);
+        opt2.addEventListener("click", szeneDreiB);
+        setInterval(countdown2, 1000);
+
+
+    }
+
+    function createButtons3(): void {
+        let opt1: HTMLElement = document.createElement("button");
+        opt1.innerHTML = "Nicht springen";
+        opt1.id = "opt1";
+        let opt2: HTMLElement = document.createElement("button");
+        opt2.innerHTML = "Springen";
+        opt2.id = "opt2";
+        auswahlBereich.appendChild(opt1);
+        auswahlBereich.appendChild(opt2);
+
+        opt1.addEventListener("click", szeneVierA);
+        opt2.addEventListener("click", szeneVierB);
+        setInterval(countdown3, 1000);
+
 
     }
 
@@ -56,7 +134,41 @@ namespace ESCAPE {
     }
 
 
+    function countdown(): void {
+        if (secondsLeft > 0 && buttonClickedControl == 0) {
+            secondsLeft--;
+            countdownBereich.innerHTML = "" + secondsLeft;
+        }
 
+        if (secondsLeft == 0) {
+            alert("Game Over");
+
+        }
+    }
+
+    function countdown2(): void {
+        if (secondsLeft2 > 0 && buttonClickedControl == 1) {
+            secondsLeft2--;
+            countdownBereich2.innerHTML = "" + secondsLeft2;
+        }
+
+        if (secondsLeft2 == 0) {
+            alert("Game Over");
+
+        }
+    }
+
+    function countdown3(): void {
+        if (secondsLeft3 > 0 && buttonClickedControl == 2) {
+            secondsLeft3--;
+            countdownBereich3.innerHTML = "" + secondsLeft3;
+        }
+
+        if (secondsLeft3 == 0) {
+            alert("Game Over");
+
+        }
+    }
 
 
 
